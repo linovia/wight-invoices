576a5c408a0955d91d020c7b934495d981469b14
Xavier Ordoquy <xordoquy@linovia.com>
2014-05-01 16:59:56 +0200

    Added the invoice application to the settings.

--- wightinvoices/settings.py
+++ wightinvoices/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project applications
+    'wightinvoices.invoice',
 )
 
 MIDDLEWARE_CLASSES = (
