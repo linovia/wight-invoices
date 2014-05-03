0de9609ebcffc1fa428712f74bd16bcda65ec2b2
Xavier Ordoquy <xordoquy@linovia.com>
2014-05-03 02:40:17 +0200

    py.test configuration files.

---
+++ conftest.py
@@ -0,0 +1,28 @@
+def pytest_configure():
+    from django.conf import settings
+
+    settings.configure(
+        DEBUG_PROPAGATE_EXCEPTIONS=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        SECRET_KEY='not very secret in tests',
+        # USE_I18N=True,
+        # USE_L10N=True,
+        # STATIC_URL='/static/',
+        # ROOT_URLCONF='tests.urls',
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.messages',
+            'django.contrib.staticfiles',
+
+            'wightinvoices',
+            'wightinvoices.invoice',
+        ),
+    )
