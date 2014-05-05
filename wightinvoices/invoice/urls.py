88f362d08a8cede41077429f9479cfa7c75918da
Xavier Ordoquy <xordoquy@linovia.com>
2014-05-06 01:02:01 +0200

    Added the creation / update views.

--- wightinvoices/invoice/urls.py
+++ wightinvoices/invoice/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^invoice/$', views.InvoiceList.as_view(), name='invoice-list'),
+    url(r'^invoice/new/$', views.InvoiceCreation.as_view(), name='invoice-new'),
     url(r'^invoice/(?P<invoice_id>\d+)/$', views.InvoiceDetail.as_view(), name='invoice-detail'),
+    url(r'^invoice/(?P<invoice_id>\d+)/update/$', views.InvoiceUpdate.as_view(), name='invoice-update'),
 )
--- wightinvoices/invoice/views.py
+++ wightinvoices/invoice/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from django.views import generic
 
 from . import models
@@ -6,6 +6,21 @@ from . import models
 
 class InvoiceList(generic.ListView):
     model = models.Invoice
+
+
+class InvoiceCreation(generic.CreateView):
+    model = models.Invoice
+
+    def get_success_url(self):
+        return reverse('invoice-detail', args=[self.object.id])
+
+
+class InvoiceUpdate(generic.UpdateView):
+    model = models.Invoice
+    pk_url_kwarg = 'invoice_id'
+
+    def get_success_url(self):
+        return reverse('invoice-detail', args=[self.object.id])
 
 
 class InvoiceDetail(generic.DetailView):
