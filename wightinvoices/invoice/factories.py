8c9f474ff7b3a3616c21b7acd68377d0e1587610
Xavier Ordoquy <xordoquy@linovia.com>
2014-06-05 00:18:00 +0200

    Added factories for User, Invoice and InvoiceItem.

--- wightinvoices/invoice/factories.py
+++ wightinvoices/invoice/factories.py
@@ -1,5 +1,14 @@
 import factory
+import django.contrib.auth.models
 from . import models
+
+
+class User(factory.DjangoModelFactory):
+    FACTORY_FOR = django.contrib.auth.models.User
+
+    first_name = 'John'
+    last_name = 'Doe'
+    # admin = False
 
 
 class Client(factory.DjangoModelFactory):
@@ -7,3 +16,19 @@ class Client(factory.DjangoModelFactory):
 
     name = factory.Sequence(lambda n: 'client%d' % n)
     address = factory.Sequence(lambda n: '%d rue de la paix' % n)
+
+
+class InvoiceItem(factory.DjangoModelFactory):
+    FACTORY_FOR = models.InvoiceItem
+
+    description = factory.Sequence(lambda n: 'Item #%d' % n)
+    quantity = 2
+    vat = 20.0
+    amount = 100.0
+
+
+class Invoice(factory.DjangoModelFactory):
+    FACTORY_FOR = models.Invoice
+
+    name = factory.Sequence(lambda n: 'Invoice #%d' % n)
+    client = factory.SubFactory(Client)
