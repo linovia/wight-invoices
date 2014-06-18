95a98bf8ded5024bd6a71c97381e060a31a539e3
Xavier Ordoquy <xordoquy@linovia.com>
2014-06-18 22:05:12 +0200

    #29: Added the urls for estimates.

--- wightinvoices/invoice/urls.py
+++ wightinvoices/invoice/urls.py
@@ -6,4 +6,9 @@ urlpatterns = patterns('',
     url(r'^invoice/new/$', views.InvoiceCreation.as_view(), name='invoice-new'),
     url(r'^invoice/(?P<invoice_id>\d+)/$', views.InvoiceDetail.as_view(), name='invoice-detail'),
     url(r'^invoice/(?P<invoice_id>\d+)/update/$', views.InvoiceUpdate.as_view(), name='invoice-update'),
+
+    url(r'^estimate/$', views.EstimateList.as_view(), name='estimate-list'),
+    url(r'^estimate/new/$', views.EstimateCreation.as_view(), name='estimate-new'),
+    url(r'^estimate/(?P<estimate_id>\d+)/$', views.EstimateDetail.as_view(), name='estimate-detail'),
+    url(r'^estimate/(?P<estimate_id>\d+)/update/$', views.EstimateUpdate.as_view(), name='estimate-update'),
 )
