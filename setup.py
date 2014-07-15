568f94212a1f5c8b8049d9067653801a1e8cd0f8
Xavier Ordoquy <xordoquy@linovia.com>
2014-07-15 07:50:47 +0200

    Created the setup file.

---
+++ setup.py
@@ -0,0 +1,41 @@
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+tests_require = [
+    'pytest',
+    'pytest-django',
+]
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='wight-invoice',
+    version='0.1.0',
+    packages=find_packages('.'),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=['logan'],
+    extras_require={
+        'tests': tests_require,
+    },
+    cmdclass={'test': PyTest},
+    entry_points={
+        'console_scripts': [
+            'wightinvoices = wightinvoices.runner:main',
+        ],
+    },
+)
