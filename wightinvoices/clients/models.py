de219b18ddf10142cb4a662217cc759447befeac
Xavier Ordoquy <xordoquy@linovia.com>
2014-09-11 08:32:41 +0200

    Started multi tenant support for clients.

--- wightinvoices/clients/models.py
+++ wightinvoices/clients/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.sites.models import Site
 
 
 class Client(models.Model):
     name = models.CharField(max_length=64)
     address = models.TextField()
+    site = models.ForeignKey(Site)
+
+    class Meta:
+        unique_together = (
+            ('name', 'site'),
+        )
 
     def __str__(self):
         return self.name
