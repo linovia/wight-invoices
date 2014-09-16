45d3ec3ef673bb1223edb85eb094588c0c1a9df3
Xavier Ordoquy <xordoquy@linovia.com>
2014-09-16 23:08:01 +0200

    Removed the FK to Site.

--- wightinvoices/clients/models.py
+++ wightinvoices/clients/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-from django.contrib.sites.models import Site
 
 
 class Client(models.Model):
     name = models.CharField(max_length=64)
-    address = models.TextField()
-    site = models.ForeignKey(Site)
+    address = models.TextField(blank=True, null=True)
 
-    class Meta:
-        unique_together = (
-            ('name', 'site'),
-        )
+    # class Meta:
+    #     unique_together = (
+    #         ('name', 'site'),
+    #     )
 
     def __str__(self):
         return self.name
