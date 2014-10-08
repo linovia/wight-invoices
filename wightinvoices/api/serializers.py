82f28c9bdda0a639684b5aa3a4793d1e37880c66
Xavier Ordoquy <xordoquy@linovia.com>
2014-10-08 19:31:31 +0200

    API upgrade to DRF 3.

--- wightinvoices/api/serializers.py
+++ wightinvoices/api/serializers.py
@@ -1,6 +1,6 @@
 from wightinvoices.invoice import models
+from wightinvoices.clients.models import Client
 from rest_framework import serializers
-
 
 
 class NestedInvoiceItem(serializers.ModelSerializer):
@@ -9,10 +9,23 @@ class NestedInvoiceItem(serializers.ModelSerializer):
         fields = ('id', 'description', 'quantity', 'vat', 'amount')
 
 
+class ListNestedInvoiceItem(serializers.ListSerializer):
+    child = NestedInvoiceItem()
+
+    def to_representation(self, data):
+        """
+        List of object instances -> List of dicts of primitive datatypes.
+        """
+        return serializers.ReturnList(
+            [self.child.to_representation(item) for item in data.all()],
+            serializer=self
+        )
+
+
 class Invoice(serializers.ModelSerializer):
-    owner = serializers.SlugRelatedField(slug_field='email')
-    client = serializers.SlugRelatedField(slug_field='name')
-    items = NestedInvoiceItem(many=True, read_only=True)
+    owner = serializers.SlugRelatedField(slug_field='email', read_only=True)
+    client = serializers.SlugRelatedField(slug_field='name', queryset=Client.objects.all())
+    items = ListNestedInvoiceItem()
 
     class Meta:
         model = models.Invoice
