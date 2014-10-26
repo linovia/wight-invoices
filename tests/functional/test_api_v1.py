a87c7d6e61a68031f0dee92c4f71df42553e20ad
Xavier Ordoquy <xordoquy@linovia.com>
2014-10-26 23:43:39 +0100

    Add invoice creation API (partial)

--- tests/functional/test_api_v1.py
+++ tests/functional/test_api_v1.py
@@ -2,7 +2,7 @@ import pytest
 from decimal import Decimal
 from django.core.urlresolvers import reverse
 from rest_framework.test import APIClient
-from wightinvoices.invoice import factories
+from wightinvoices.invoice import factories, models
 
 
 pytestmark = pytest.mark.django_db
@@ -13,7 +13,8 @@ def test_invoice_detail():
     owner = factories.User.create(password="clear$abc$toto")
     assert api_client.login(username=owner.username, password="toto")
 
-    invoice = factories.Invoice.create()
+    client = factories.Client.create()
+    invoice = factories.Invoice.create(client=client)
     invoice_item = factories.InvoiceItem.create(invoice=invoice)
 
     url = reverse('api-invoice-detail', kwargs={'pk': invoice.id})
@@ -34,3 +35,77 @@ def test_invoice_detail():
         'owner': owner.email,
         'status': 'draft',
     }
+
+
+def test_invoice_create():
+    """
+    Regular invoice creation
+    """
+    owner = factories.User.create(password="clear$abc$toto")
+    assert api_client.login(username=owner.username, password="toto")
+
+    client = factories.Client.create(name='myclient')
+
+    invoice_data = {
+        'client': 'myclient',
+        'notes': 'N/A',
+        'status': 'draft',
+        'items': [{
+            'description': 'qwerty',
+            'quantity': 5,
+            'vat': '20.00',
+            'amount': '100.00',
+        }]
+    }
+
+    url = reverse('api-invoice-list')
+    response = api_client.post(url, data=invoice_data)
+
+    assert response.status_code == 201, response.data
+
+    invoice = models.Invoice.objects.order_by('-id').first()
+    assert invoice.notes == 'N/A'
+    assert invoice.client.name == 'myclient'
+    assert invoice.owner_id == owner.id
+    assert invoice.status == 'draft'
+    assert invoice.items.count() == 1
+    item = invoice.items.all()[0]
+    assert item.quantity == 5
+
+
+def test_invoice_create_for_another_owner():
+    """
+    We need to make sure we can't create an invoice on behalf of someone else
+    """
+    owner = factories.User.create(password="clear$abc$toto")
+    assert api_client.login(username=owner.username, password="toto")
+
+    non_owner = factories.User.create()
+    client = factories.Client.create(name='myclient')
+
+    invoice_data = {
+        'client': 'myclient',
+        'notes': 'N/A',
+        'status': 'draft',
+        'owner': 'non_owner',
+        'items': [{
+            'description': 'qwerty',
+            'quantity': 5,
+            'vat': '20.00',
+            'amount': '100.00',
+        }]
+    }
+
+    url = reverse('api-invoice-list')
+    response = api_client.post(url, data=invoice_data)
+
+    assert response.status_code == 201, response.data
+
+    invoice = models.Invoice.objects.order_by('-id').first()
+    assert invoice.notes == 'N/A'
+    assert invoice.client.name == 'myclient'
+    assert invoice.owner_id == owner.id
+    assert invoice.status == 'draft'
+    assert invoice.items.count() == 1
+    item = invoice.items.all()[0]
+    assert item.quantity == 5
--- wightinvoices/api/serializers.py
+++ wightinvoices/api/serializers.py
@@ -25,8 +25,23 @@ class ListNestedInvoiceItem(serializers.ListSerializer):
 class Invoice(serializers.ModelSerializer):
     owner = serializers.SlugRelatedField(slug_field='email', read_only=True)
     client = serializers.SlugRelatedField(slug_field='name', queryset=Client.objects.all())
-    items = ListNestedInvoiceItem()
+    # items = ListNestedInvoiceItem()
+    items = NestedInvoiceItem(many=True)
 
     class Meta:
         model = models.Invoice
         fields = ('id', 'notes', 'owner', 'client', 'status', 'items')
+
+        extra_kwargs = {
+            'owner': {'read_only': True}
+        }
+
+    def create(self, validated_data):
+        print(validated_data)
+        items_data = validated_data.pop('items', [])
+        validated_data['owner'] = self.context['request'].user
+        invoice = models.Invoice.objects.create(**validated_data)
+        for item_data in items_data:
+            print('Creating: %s' % item_data)
+            models.InvoiceItem.objects.create(invoice=invoice, **item_data)
+        return invoice
